aae31ad2e02ee0fe7c8c89909bac8a0bbc744f8e
Oleg <oleg.denkos@mail.ru>
2018-12-02 18:45:41 +0300

    farad bonsai

--- config/initializers/chewy.rb
+++ config/initializers/chewy.rb
@@ -1,2 +1,1 @@
-Chewy.settings = {host: 'http://paas:d343a38b7f81439a2299c127edd732d2@thorin-us-east-1.searchly.com
-'}
+Chewy.settings = {host: 'https://cherry-813679676.us-east-1.bonsaisearch.net/'}
